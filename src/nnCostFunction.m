66876e76e1c57782ffcf16419980a723508a0834
fardeenmohd <fardeenmohdpl@gmail.com>
2017-11-07 03:05:42 +0100

    cleanup and git test from matlab

--- src/nnCostFunction.m
+++ src/nnCostFunction.m
@@ -30,63 +30,19 @@ J = 0;
 Theta1_grad = zeros(size(Theta1));
 Theta2_grad = zeros(size(Theta2));
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: You should complete the code by working through the
-%               following parts.
-%
-% Part 1: Feedforward the neural network and return the cost in the
-%         variable J. After implementing Part 1, you can verify that your
-%         cost function computation is correct by verifying the cost
-%         computed in ex4.m
-%
-% Part 2: Implement the backpropagation algorithm to compute the gradients
-%         Theta1_grad and Theta2_grad. You should return the partial derivatives of
-%         the cost function with respect to Theta1 and Theta2 in Theta1_grad and
-%         Theta2_grad, respectively. After implementing Part 2, you can check
-%         that your implementation is correct by running checkNNGradients
-%
-%         Note: The vector y passed into the function is a vector of labels
-%               containing values from 1..K. You need to map this vector into a 
-%               binary vector of 1's and 0's to be used with the neural network
-%               cost function.
-%
-%         Hint: We recommend implementing backpropagation using a for-loop
-%               over the training examples if you are implementing it for the 
-%               first time.
-%
-% Part 3: Implement regularization with the cost function and gradients.
-%
-%         Hint: You can implement this around the code for
-%               backpropagation. That is, you can compute the gradients for
-%               the regularization separately and then add them to Theta1_grad
-%               and Theta2_grad from Part 2.
-%
-
 
 % Feed forward algorithm to get output of neural network h(x)
 z2 = X * Theta1';
 a2 = z2;
 % Each row of a2 contains the node values of the second layer (a21, a22, a23...) corresponding
-% to an input row from X (all 5000 examples)
+% to an input row from X 
 
 % Add intercept term to a2 before we find a3
-% size(a2)
+
 a2 = [ones(size(a2, 1), 1) a2];
 z3 = a2 * Theta2';
 a3 = z3;
-% size(a3)
-% size(Theta2)
-% Recode y so that it is a vector of 0s and 1s instead of expected number
-% temp = zeros(size(y, 1), num_labels);
-% for i = 1:size(y,1)
-%     row = temp(i, :);
-%     row(y(i)) = 1;
-%     temp(i, :) = row;
-% end
-% y = temp;
-% size(a3)
-% size(y)
-%J = ((1 / m) * sum(sum((-y) * (log(a3)') - ((1 - y) * (log(1 - a3)')), 2)));
+
 J = zeros(m, 1);
 for i = 1:size(y,1)
    rowy = y(i,:);
@@ -111,7 +67,7 @@ for i = 1:m
     rowy = y(i, :);
     rowz2 = z2(i, :);
     delta3 = rowa3 - rowy;
-    % sigmoid_gradient2 = sigmoidGradient(rowz2);
+   
     delta2 = Theta2' * delta3';
     % Remove delta2_0
     delta2 = delta2(2:end);
